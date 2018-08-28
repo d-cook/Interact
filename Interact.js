b2c224d17f70186fad8a05cfebd4e70dc3b8b979
dcook <dcook0819@gmail.com>
2018-08-28 00:34:38 -0700

    Added a lot of base operations to "root" context

--- Interact.js
+++ Interact.js
@@ -1,12 +1,58 @@
+function has (o, p   ) { try { return Object.prototype.hasOwnProperty.call(o, p); } catch(e) { return false; } }
+function get (o, p   ) { return has(o, p) ? o[p] : null; }
+function set (o, p, v) { var t = type(o); if (t === 'object' || t === 'array') { o[p] = v; } return v; }
+function del (o, p   ) { var v = (has(o, p) ? o[p] : null); delete o[p]; return v; }
+
 function type(o) {
     return (o === null || typeof o === 'undefined') ? 'null' :
            Array.isArrray(o)                        ? 'array'
                                                     : typeof o;
 }
 
-function get(context, key) {
-    return (context) ? context[key] : null;
+function _if (cond, T, F) {
+    return apply(truthy(cond) ? T : F, [cond]);
 }
+
+function and (L, R) {
+    if (arguments.length < 2 || not(L)) { return L; }
+    var args = [apply(R, [])];
+    args.push.apply(args, slice(arguments, 2));
+    return and.apply(null, args);
+}};
+
+function or (L, R) {
+    if (arguments.length < 2 || truthy(L)) { return L; }
+    var args = [apply(R, [])];
+    args.push.apply(args, slice(arguments, 2));
+    return or.apply(null, args);
+}};
+
+function newObj ( ) { return Object.create(null); }
+function keys   (o) { return Object.keys(o||{}) || []; }
+function length (o) { return(Object.keys(o||{}) || []).length; }
+function truthy (v) { return v || v === 0 || v === ''; }
+function not    (v) { return !truthy(v); }
+
+function plus () { var r = arguments[0]; for(var i=1; i<arguments.length; i++) {       r +=      arguments[i];                 } return r;    }
+function minus() { var r = arguments[0]; for(var i=1; i<arguments.length; i++) {       r -=      arguments[i];                 } return r;    }
+function mult () { var r = arguments[0]; for(var i=1; i<arguments.length; i++) {       r *=      arguments[i];                 } return r;    }
+function div  () { var r = arguments[0]; for(var i=1; i<arguments.length; i++) {       r /=      arguments[i];                 } return r;    }
+function mod  () { var r = arguments[0]; for(var i=1; i<arguments.length; i++) {       r %=      arguments[i];                 } return r;    }
+function EQ   () { var r = arguments[0]; for(var i=1; i<arguments.length; i++) { if (!(r ===(    arguments[i]))) return false; } return true; }
+function NE   () { var r = arguments[0]; for(var i=1; i<arguments.length; i++) { if (!(r !==(    arguments[i]))) return false; } return true; }
+function LT   () { var r = arguments[0]; for(var i=1; i<arguments.length; i++) { if (!(r <  (r = arguments[i]))) return false; } return true; }
+function GT   () { var r = arguments[0]; for(var i=1; i<arguments.length; i++) { if (!(r >  (r = arguments[i]))) return false; } return true; }
+function LTE  () { var r = arguments[0]; for(var i=1; i<arguments.length; i++) { if (!(r <= (r = arguments[i]))) return false; } return true; }
+function GTE  () { var r = arguments[0]; for(var i=1; i<arguments.length; i++) { if (!(r >= (r = arguments[i]))) return false; } return true; }
+
+function slice  (a,b,e) { return (type(a) !== 'array') ? null : [].slice  .apply(a, [].slice.call(arguments, 1)); }
+function push   (a    ) { return (type(a) !== 'array') ? null : [].push   .apply(a, [].slice.call(arguments, 1)); }
+function unshift(a    ) { return (type(a) !== 'array') ? null : [].unshift.apply(a, [].slice.call(arguments, 1)); }
+function pop    (a    ) { return (type(a) !== 'array') ? null : [].pop    .apply(a); }
+function shift  (a    ) { return (type(a) !== 'array') ? null : [].shift  .apply(a); }
+
+function charAt    (s,i  ) { return (type(s) !== 'string') ? null : s.charAt   (i   ); }
+function substring (s,b,e) { return (type(s) !== 'string') ? null : s.substring(b, e); }
 
 function lookupValue(context, path) {
     return (path.length > 0) ? lookupValue(get(context, path[0]), path.slice(1))
@@ -70,6 +116,12 @@ function apply(func, args) {
 
 var root = {
     parent: null,
-    values: [root, type, get, lookupValue, lookupContext, lookup, evalObject, evalArray, evalCall, evalCalc, apply],
-    args: []
+    args: [],
+    values: [
+        root,
+        lookupValue, lookupContext, lookup, evalObject, evalArray, evalCall, evalCalc, apply,
+        has, get, set, del, type, _if, and, or, newObj, keys, length, truthy, not,
+        plus, minus, mult, div, mod, EQ, NE, LT, GT, LTE, GTE,
+        slice, push, unshift, pop, shift, charAt, substring,
+    ]
 };
